f61764d89aa30e93f2b7027b662a7c2c2e0878dc
liklon <835705302@qq.com>
2017-12-04 16:16:20 +0800

    complete all function

--- TEA_Encrypt.h
+++ TEA_Encrypt.h
@@ -3,12 +3,10 @@
  * Author: Bean 
  * Email: notrynohigh@outlook.com
  * Every one can use this file free !
- ******************************************************************************
  ******************************************************************************/
-
-
-#ifndef __TEA_ENCRYPT__
-#define __TEA_ENCRYPT__
+ 
+#ifndef __TEA_ENCRYPT_H__
+#define __TEA_ENCRYPT_H__
 
 /******************************************************************************
  *  basic data type
@@ -19,9 +17,6 @@ typedef unsigned short TEA_U16;
 typedef signed short   TEA_S16;
 typedef unsigned int   TEA_U32;
 typedef signed int     TEA_S32;
-
-typedef unsigned long long int   TEA_U64;
-typedef signed long long int     TEA_S64;
 
 #define TEA_NULL    ((void *)0)
 /******************************************************************************
@@ -47,51 +42,14 @@ typedef enum
 }TEA_ErrorCode_t;
 
 /******************************************************************************
- *  typedef struct
- ******************************************************************************/
-typedef struct
-{
-	TEA_U32 n;
-	TEA_U32 e;
-}TEA_PublicKey_t;
-
-typedef struct
-{
-	TEA_U32 n;
-	TEA_U32 d;
-}TEA_PrivateKey_t;
-
-
-/******************************************************************************
  * public functions
  ******************************************************************************/
 
-TEA_ErrorCode_t TEA_CreateKey(TEA_U32 prime_1, TEA_U32 prime_2, TEA_U32 e);
+TEA_ErrorCode_t TEA_Config128bitsKey(TEA_U8 *key);
+TEA_ErrorCode_t TEA_ConfigEncryptTimes(TEA_U8 t);
 
-TEA_ErrorCode_t TEA_ConfigPublicKey(TEA_U32 n, TEA_U32 e);
-TEA_ErrorCode_t TEA_ConfigPrivateKey(TEA_U32 n, TEA_U32 d);
-
-/**
- * TEA encrypt
- * text: the data which you want to encrypt.
- * result: the result of this function 
- * Len: the length of text .
- * return : the length of result . 
- *        0 : error 
- */
-TEA_U32 TEA_Encrypt(TEA_U8 *text , TEA_U8 *result, TEA_U32 len);
-
-/**
- * TEA decrypt
- * text: the data which you want to decrypt.
- * result: the result of this function 
- * Len: the length of text .
- * return : the length of result . 
- *        0 : error 
- */
-TEA_U32 TEA_Decrypt(TEA_U8 *text, TEA_U8 *result, TEA_U32 len);
-
-
+TEA_ErrorCode_t TEA_Encrypt(TEA_U8 *text, TEA_U32 size);
+TEA_ErrorCode_t TEA_Decrypt(TEA_U8 *text, TEA_U32 size);
 
 #endif
 /******************************************************************************
